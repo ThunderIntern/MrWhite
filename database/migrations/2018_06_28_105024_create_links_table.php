9f89bfbb4757de07c36115fda00475000960bd9d
trherla <tri.h.is.534@gmail.com>
2018-06-29 09:28:39 +0700

    .

--- database/migrations/2018_06_28_105024_create_links_table.php
+++ database/migrations/2018_06_28_105024_create_links_table.php
@@ -17,11 +17,7 @@ class CreateLinksTable extends Migration
             $table->increments('id');
             $table->string('tag');
             $table->text('link');
-<<<<<<< HEAD:database/migrations/2018_06_21_023343_tbl_link_catalog.php
-            $table->string('id_catalog');
-=======
             $table->string('catalog_id');
->>>>>>> 431a8d834cc90b4805d94d04c1c83105612f712b:database/migrations/2018_06_28_105024_create_links_table.php
         });
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,9 +14,6 @@ class DatabaseSeeder extends Seeder
 	     $this->call(CatalogsTableSeeder::class);
 	     $this->call(CategoriesTableSeeder::class);
          $this->call(LinksTableSeeder::class);
-         // $this->call(seeder_banner::class);
-         // $this->call(seeder_link_catalog::class);
-         // $this->call(seeder_setting_web::class);
          $this->call(seeder_users::class);
     }
 }
