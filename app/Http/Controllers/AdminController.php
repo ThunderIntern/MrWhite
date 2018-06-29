83478e6625ddfaab9b423a5ed4dcd510a675949a
kikirizki <kikirizkiamalinda@gmail.com>
2018-06-29 13:55:09 +0700

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,6 +17,46 @@ class AdminController extends Controller
             return view('admin/dashboard');
         }
     }
+    public function dataProduk(){
+        if(!Session::get('login')){
+            return redirect('login')->with('alert','Kamu harus login dulu');
+        }
+        else{
+            return view('admin/katalog/dataProduk');
+        }
+    }
+    public function dataKategori(){
+        if(!Session::get('login')){
+            return redirect('login')->with('alert','Kamu harus login dulu');
+        }
+        else{
+            return view('admin/katalog/dataKategori');
+        }
+    }
+    public function produkBaru(){
+        if(!Session::get('login')){
+            return redirect('login')->with('alert','Kamu harus login dulu');
+        }
+        else{
+            return view('admin/katalog/produkBaru');
+        }
+    }
+    public function homePage(){
+        if(!Session::get('login')){
+            return redirect('login')->with('alert','Kamu harus login dulu');
+        }
+        else{
+            return view('admin/webSetting/homepage');
+        }
+    }
+    public function component(){
+        if(!Session::get('login')){
+            return redirect('login')->with('alert','Kamu harus login dulu');
+        }
+        else{
+            return view('admin/webSetting/component');
+        }
+    }
     public function login(){
         return view('login');
     }
@@ -28,6 +68,8 @@ class AdminController extends Controller
         if($data==1){ //apakah email tersebut ada atau tidak
           $data = ModelUser::where([['email',$email],['password',$password]])->first();
           Session::put('email',$data->email);
+          Session::put('nama',$data->nama);
+          Session::put('foto',$data->foto);
           Session::put('login',TRUE);
           return redirect('admin');
 
